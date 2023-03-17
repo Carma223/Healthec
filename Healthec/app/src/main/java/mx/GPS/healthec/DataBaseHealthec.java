0c3f1a6ef630fa401d9cc64b8932690a0ee5d25d
Fernando Pérez Romero <alu.19130959@correo.itlalaguna.edu.mx>
2023-03-17 11:17:01 -0600

    cambios

--- Healthec/app/src/main/java/mx/GPS/healthec/DataBaseHealthec.java
+++ Healthec/app/src/main/java/mx/GPS/healthec/DataBaseHealthec.java
@@ -13,10 +13,28 @@ import java.util.List;
 
 public class DataBaseHealthec extends SQLiteOpenHelper {
 
-    public static final String USER_TABLE = "USER_TABLE";
-    public static final String COLUMN_USER_EMAIL = "USER_EMAIL";
-    public static final String COLUMN_USER_PASSWORD = "USER_PASSWORD";
-    public static final String COLUMN_ID = "ID";
+    public static final String TABLA_USUARIO = "TABLA_USUARIO";
+    public static final String COLUMNA_USUARIO_CORREO = "USUARIO_CORREO";
+    public static final String COLUMNA_USUARIO_CLAVE = "USUARIO_CLAVE";
+    public static final String COLUMNA_USUARIO_ID = "USUARIO_ID";
+
+    public static final String TABLA_RECORDATORIOS = "TABLA_RECORDATORIOS";
+    public static final String COLUMNA_RECORDATORIOS_CLINICA = "COLUMNA_RECORDATORIOS_CLINICA";
+    public static final String COLUMNA_RECORDATORIOS_MEDICO = "COLUMNA_RECORDATORIOS_MEDICOS";
+    public static final String COLUMNA_RECORDATORIOS_FECHAHORA = "COLUMNA_RECORDATORIOS_FECHAHORA";
+    public static final String COLUMNA_RECORDATORIOS_ID = "COLUMNA_RECORDATORIOS_ID";
+    public static final String COLUMNA_HORARIOSUEÑO_ID = "COLUMNA_HORARIOSUEÑO_ID";
+    public static final String TABLA_HORARIOSUEÑO = "TABLA_HORARIOSUEÑO ";
+    public static final String COLUMNA_HORARIOSUEÑO_DIA = "COLUMNA_HORARIOSUEÑO_DIA";
+    public static final String COLUMNA_HORARIOSUEÑO_HORA = "COLUMNA_HORARIOSUEÑO_HORA";
+    public static final String TABLA_RECETARIOS = "TABLA_RECETARIOS";
+    public static final String COLUMNA_RECETARIOS_ID = "COLUMNA_RECETARIOS_ID";
+    public static final String COLUMNA_RECETARIOS_RECETA = "COLUMNA_RECETARIOS_RECETA";
+    public static final String COLUMNA_RECETARIOS_PASOS = "COLUMNA_RECETARIOS_PASOS";
+    public static final String TABLA_CONSEJOS = "TABLA_CONSEJOS";
+    public static final String COLUMNA_CONSEJOS_ID = "COLUMNA_CONSEJOS_ID";
+    public static final String COLUMNA_CONSEJOS_DESCRIPCION = "COLUMNA_CONSEJOS_DESCRIPCION";
+
 
     public DataBaseHealthec(@Nullable Context context) {
         super(context, "healthec.db", null, 1);
@@ -26,8 +44,16 @@ public class DataBaseHealthec extends SQLiteOpenHelper {
     //una nueba db
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTableStatement = "CREATE TABLE "+ USER_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_USER_EMAIL + " TEXT, " + COLUMN_USER_PASSWORD + " TEXT)";
-        db.execSQL(createTableStatement);
+        String createTableUsuario = "CREATE TABLE "+ TABLA_USUARIO + " (" + COLUMNA_USUARIO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMNA_USUARIO_CORREO + " TEXT, " + COLUMNA_USUARIO_CLAVE + " TEXT)";
+        String createTableRecordatorios =  "CREATE TABLE "+ TABLA_RECORDATORIOS + " (" + COLUMNA_RECORDATORIOS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMNA_RECORDATORIOS_MEDICO + " TEXT, " + COLUMNA_RECORDATORIOS_FECHAHORA + " TEXT,"+COLUMNA_RECORDATORIOS_CLINICA+"TEXT)";
+        String createTableRecetarios = "CREATE TABLE " + TABLA_RECETARIOS + " (" + COLUMNA_RECETARIOS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMNA_RECETARIOS_RECETA + " TEXT, " + COLUMNA_RECETARIOS_PASOS + " TEXT)";
+        String createTableHorarioSueño = "CREATE TABLE " + TABLA_HORARIOSUEÑO + "( " + COLUMNA_HORARIOSUEÑO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMNA_HORARIOSUEÑO_DIA + " TEXT, " + COLUMNA_HORARIOSUEÑO_HORA + " TEXT)";
+        String createTableConsejos = "CREATE TABLE " + TABLA_CONSEJOS + " (" + COLUMNA_CONSEJOS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMNA_CONSEJOS_DESCRIPCION + " TEXT)";
+        db.execSQL(createTableUsuario);
+        db.execSQL(createTableConsejos);
+        db.execSQL(createTableRecetarios);
+        db.execSQL(createTableRecordatorios);
+        db.execSQL(createTableHorarioSueño);
     }
     //Este metodo es llamado si la version de la base de datos cambia. Previene que los usuarios que
     //tengan una version anterior de la bd creasheen cuando se hacen cambios a la db
@@ -41,10 +67,10 @@ public class DataBaseHealthec extends SQLiteOpenHelper {
 
         ContentValues cv = new ContentValues();
 
-        cv.put(COLUMN_USER_EMAIL, user.getEmail());
-        cv.put(COLUMN_USER_PASSWORD, user.getPassword());
+        cv.put(COLUMNA_USUARIO_CORREO, user.getEmail());
+        cv.put(COLUMNA_USUARIO_CLAVE, user.getPassword());
 
-        long insert = db.insert(USER_TABLE, null , cv );
+        long insert = db.insert(TABLA_USUARIO, null , cv );
         if( insert == -1){
             return false;
         } else {
@@ -56,7 +82,7 @@ public class DataBaseHealthec extends SQLiteOpenHelper {
     public List<UserModel> getEveryone() {
         List<UserModel> returnList = new ArrayList<>();
         //Toma la información de la db
-        String queryString = "SELECT * FROM " + USER_TABLE;
+        String queryString = "SELECT * FROM " + TABLA_USUARIO;
 
         SQLiteDatabase db = this.getReadableDatabase();
 
@@ -83,8 +109,8 @@ public class DataBaseHealthec extends SQLiteOpenHelper {
     }
 
     public boolean Exists( UserModel user ){
-        String queryString = "SELECT " + COLUMN_ID + " FROM " + USER_TABLE + "WHERE " + COLUMN_USER_EMAIL + " = " + user.getEmail() +
-                " AND " + COLUMN_USER_PASSWORD + " = " + user.getPassword();
+        String queryString = "SELECT " + COLUMNA_USUARIO_ID + " FROM " + TABLA_USUARIO + "WHERE " + COLUMNA_USUARIO_CORREO + " = " + user.getEmail() +
+                " AND " + COLUMNA_USUARIO_CLAVE + " = " + user.getPassword();
 
         SQLiteDatabase db = this.getReadableDatabase();
 
