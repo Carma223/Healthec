87e41d92108477946cf0713159b11a5d09050a0e
Fernando Pérez Romero <alu.19130959@correo.itlalaguna.edu.mx>
2023-03-24 11:49:25 -0600

    Menu

--- Healthec/app/src/main/java/mx/GPS/healthec/MenuActivity.java
+++ Healthec/app/src/main/java/mx/GPS/healthec/MenuActivity.java
@@ -1,5 +1,6 @@
 package mx.GPS.healthec;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,21 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnModuloSueñoClick (){
+        startActivity(new Intent( this, SleepActivity.class));
+    }
+
+    public void btnModuloRecetasClick (){
+        startActivity(new Intent( this, RecetasActivity.class));
+    }
+
+    public void btnModuloRecordatorioClick(){
+        startActivity(new Intent( this, RecetasActivity.class));
+    }
+
+    public void btnModuloMeditacionClick(){
+        startActivity(new Intent( this, MeditacionActivity.class));
     }
 }
