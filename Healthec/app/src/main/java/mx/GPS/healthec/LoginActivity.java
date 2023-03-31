2a71f2a917fd0c8b92a41954254ae52e20b1982d
Fernando Pérez Romero <alu.19130959@correo.itlalaguna.edu.mx>
2023-03-31 11:30:04 -0600

    Revert "InterfazMeditacion"
    This reverts commit 0b523ad1010eb7ed6c27aca1b31de626e939a869.

--- Healthec/app/src/main/java/mx/GPS/healthec/LoginActivity.java
+++ Healthec/app/src/main/java/mx/GPS/healthec/LoginActivity.java
@@ -1,5 +1,8 @@
 package mx.GPS.healthec;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,9 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
