f41dac21dac5e85b4fc12312e183d3bd11f85aa3
Fernando Pérez Romero <alu.19130959@correo.itlalaguna.edu.mx>
2023-03-31 11:35:22 -0600

    Revert "Revert "InterfazMeditacion""
    This reverts commit 2a71f2a917fd0c8b92a41954254ae52e20b1982d.

--- Healthec/app/src/main/java/mx/GPS/healthec/MeditacionActivity.java
+++ Healthec/app/src/main/java/mx/GPS/healthec/MeditacionActivity.java
@@ -3,6 +3,9 @@ package mx.GPS.healthec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class MeditacionActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MeditacionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meditacion);
+        ImageView imageView = findViewById(R.id.imageView4);
+        float alphaValue = 0.5f; // establece el valor de transparencia entre 0 (totalmente transparente) y 1 (totalmente opaco)
+        imageView.setAlpha(alphaValue);
+
     }
+
+    //boton para entrar a la seccion conciliacion del sueño
+    public void btnSueño( View v ) { setContentView(R.layout.activity_sueño);
+
+    }
+    //boton para entrar a la seccion alvio de ansiendad
+    public void btnAnsiedad( View v ) { setContentView(R.layout.activity_ansiedad);
+
+    }
+    //boton para entrar a la seccion relajacion vespertina
+    public void btnRelajacion( View v ) { setContentView(R.layout.activity_relajacion);
+
+    }
+    //boton para entrar a la seccion de alivio de sobrepensamiento
+    public void btnAlivio( View v ) {setContentView(R.layout.activity_alivio);
+
+    }
+
+
+    //boton para regresar al menu principal
+    public void btnRegresar( View v ) {
+        setContentView(R.layout.activity_menu);
+    }
+
 }
