31e807544c5890d6e784915b23b9cd668899e344
Fernando Pérez Romero <alu.19130959@correo.itlalaguna.edu.mx>
2023-05-12 01:27:43 -0600

    Actualizacion

--- Healthec/app/src/main/java/mx/GPS/healthec/modelos/Recetas.java
+++ Healthec/app/src/main/java/mx/GPS/healthec/modelos/Recetas.java
@@ -1,21 +1,16 @@
 package mx.GPS.healthec.modelos;
 
-import android.os.Parcel;
-
-
-public class Ingredientes {
+public class Recetas {
     private String Titulo;
-    private int Imagen, Clave;
+    private String Descripcion;
+    private int Imagen;
+    private int clave;
 
-    public Ingredientes(String titulo, int imagen, int clave) {
+    public Recetas(String titulo, String descripcion, int imagen, int clave) {
         Titulo = titulo;
+        Descripcion = descripcion;
         Imagen = imagen;
-        Clave = clave;
-    }
-    protected Ingredientes(Parcel in){
-        Titulo = in.readString();
-        Imagen = in.readInt();
-        Clave = in.readInt();
+        this.clave = clave;
     }
 
     public String getTitulo() {
@@ -24,6 +19,14 @@ public class Ingredientes {
 
     public void setTitulo(String titulo) {
         Titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
     }
 
     public int getImagen() {
@@ -35,11 +38,10 @@ public class Ingredientes {
     }
 
     public int getClave() {
-        return Clave;
+        return clave;
     }
 
     public void setClave(int clave) {
-        clave = clave;
+        this.clave = clave;
     }
-
 }
