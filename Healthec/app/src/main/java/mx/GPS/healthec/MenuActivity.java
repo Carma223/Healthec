f7cb2afae4740c822f2f9acf9a6cc113ee36f649
Carma223 <79612233+Carma223@users.noreply.github.com>
2023-05-15 21:21:49 -0600

    Revert "Merge branch 'main' of https://github.com/Carma223/Healthec"
    This reverts commit b8708eec5ed938e3b2a3244f333a2328ccaee271, reversing
    changes made to 524fce343df529d3b1324b56c7f2c44153a6a48c.

--- Healthec/app/src/main/java/mx/GPS/healthec/MenuActivity.java
+++ Healthec/app/src/main/java/mx/GPS/healthec/MenuActivity.java
@@ -2,13 +2,10 @@ package mx.GPS.healthec;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
@@ -21,23 +18,6 @@ public class MenuActivity extends AppCompatActivity {
 
     private static final String FILE_NAME = "Recordatorios.txt";
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.Random;
-
-public class MenuActivity extends AppCompatActivity {
-
-    private ImageView perfil;
-
-    private TextView txtvFrase;
-
-    private DatabaseReference mDataBase;
-
-    private Random azar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,37 +40,6 @@ public class MenuActivity extends AppCompatActivity {
         } else {
             textView4.setText("No hay recordatorios disponibles");
         }
-        perfil = findViewById(R.id.imgvPerfil);
-        perfil.setImageResource(R.drawable.saludconsejo);
-        txtvFrase = findViewById(R.id.txtvFrase);
-
-        mDataBase = FirebaseDatabase.getInstance().getReference();
-
-        azar = new Random();
-
-       int fraseAzar = azar.nextInt(10) +1;
-
-
-
-        mDataBase.child("frases").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                if(snapshot.exists()){
-                    // String consejo = snapshot.getValue().toString();
-                    String frase = snapshot.child(fraseAzar+"").getValue().toString();
-                    txtvFrase.setText(frase);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-
-
     }
 
     public void btnModuloSueñoClick (View v){
@@ -99,7 +48,7 @@ public class MenuActivity extends AppCompatActivity {
     }
 
     public void btnModuloRecetasClick (View v){
-        startActivity(new Intent( MenuActivity.this, IngredientesActivity.class));
+        startActivity(new Intent( MenuActivity.this, RecetasActivity.class));
     }
 
     public void btnModuloRecordatorioClick(View v){
--- Healthec/app/src/main/java/mx/GPS/healthec/RecetasActivity.java
+++ Healthec/app/src/main/java/mx/GPS/healthec/RecetasActivity.java
@@ -1,114 +1,32 @@
 package mx.GPS.healthec;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.SearchView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import org.checkerframework.checker.units.qual.A;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-import mx.GPS.healthec.Adapter.RecetasAdapter;
-import mx.GPS.healthec.modelos.Ingredientes;
-import mx.GPS.healthec.modelos.Recetas;
-
-public class RecetasActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
-    public final int [][] imagenes = {{0, R.drawable.spagueti, R.drawable.chilaquiles,
-                                    R.drawable.sopafid, R.drawable.enchiladaroja,
-                                    R.drawable.salsatomate},
-                                    {0, R.drawable.pastaqueso, R.drawable.lasana,
-                                     R.drawable.espaguetichipotle, R.drawable.espaguetiblanco,
-                                     R.drawable.espaguetiespinaca},
-                                    {0, R.drawable.gaspachofresa, R.drawable.manzanamicro,
-                                     R.drawable.chia, R.drawable.tartapina,
-                                     R.drawable.tartamanzana},
-                                    {0, R.drawable.ensaladaatun, R.drawable.ensaladapollo,
-                                     R.drawable.ensaladamandarina, R.drawable.ensaladaaguacate,
-                                     R.drawable.ensaladatomate},
-                                    {0, R.drawable.costillaadobo, R.drawable.costillachile,
-                                     R.drawable.ribeye, R.drawable.calabazaarra,
-                                     R.drawable.medallones},
-                                    {0, R.drawable.hampollo, R.drawable.tacospollo,
-                                     R.drawable.pollocazuela, R.drawable.pastapollo,
-                                     R.drawable.palopollo},
-                                    {0, R.drawable.chilaverdes, R.drawable.molleterojo,
-                                    R.drawable.huevonido, R.drawable.huevoranchero,
-                                    R.drawable.panfrances},
-                                    {0, R.drawable.pescadoajillo, R.drawable.pescadotalla,
-                                     R.drawable.pescadoculi, R.drawable.pescadoempa,
-                                     R.drawable.pescadozar}};
-    private int ingredienteClave;
-    private RecyclerView recvRecetas;
-    private SearchView srcvRecetas;
-    DatabaseReference mDatabase;
-    private RecetasAdapter rAdapter;
-    private ArrayList<Recetas> mRecetasList = new ArrayList<Recetas>();
-
+public class RecetasActivity extends AppCompatActivity {
+    //Constantes
+    ListView lv_recetasList;
+    ArrayAdapter recetasArrayAdapter;
+    DataBaseHealthec db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recetas);
-        srcvRecetas = findViewById(R.id.srcvRecetas);
-        recvRecetas = findViewById(R.id.recvRecetas);
-        recvRecetas.setLayoutManager(new LinearLayoutManager(this));
-        ingredienteClave = getIntent().getIntExtra("clave",0);
-        actualizaImagenes();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        getRecetasFromFirebase();
-        srcvRecetas.setOnQueryTextListener(this);
-    }
 
-    private void getRecetasFromFirebase(){
-        mDatabase.child("Ingredientes/"+ingredienteClave+"/Recetas").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
-                    for(DataSnapshot ds: snapshot.getChildren()){
-                        String Titulo = ds.child("Titulo").getValue().toString();
-                        String Descripcion = ds.child("Descripcion").getValue().toString();
-                        int Imagen = (int)(long) ds.child("Imagen").getValue();
-                        int Clave = (int)(long) ds.child("Clave").getValue();
-                        mRecetasList.add(new Recetas(Titulo,Descripcion,Imagen,Clave));
-                    }
-                    rAdapter = new RecetasAdapter(mRecetasList);
-                    recvRecetas.setAdapter(rAdapter);
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+        //ListView
+        lv_recetasList = findViewById(R.id.lv_recetas);
 
-            }
-        });
+        //Llenado del ViewList
+        db = new DataBaseHealthec(RecetasActivity.this);
+        recetasArrayAdapter = new ArrayAdapter<RecetasModel>(RecetasActivity.this, android.R.layout.simple_list_item_1, db.getRecipes());
+        lv_recetasList.setAdapter(recetasArrayAdapter);
 
-    }
-
-    private void actualizaImagenes(){
-        mDatabase = FirebaseDatabase.getInstance().getReference("Ingredientes/"+ingredienteClave+"/Recetas");
-        for(int i = 1; i < imagenes[ingredienteClave-1].length; i++){
-            Map<String,Object> imagenesMap = new HashMap<>();
-            imagenesMap.put("Imagen",imagenes[ingredienteClave-1][i]);
-            mDatabase.child(i+"").updateChildren(imagenesMap);
-        }
-    }
-
-    @Override
-    public boolean onQueryTextSubmit(String s) {
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String s) {
-        rAdapter.filtrado(s);
-        return false;
     }
 }
