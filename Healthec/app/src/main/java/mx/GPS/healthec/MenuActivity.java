eaa8970c5dd34a109b2099a4895d9b085d370980
carlossromoo <daniielromo14@gmail.com>
2023-05-21 06:14:14 -0600

    Cambios en las interfaces

--- Healthec/app/src/main/java/mx/GPS/healthec/MenuActivity.java
+++ Healthec/app/src/main/java/mx/GPS/healthec/MenuActivity.java
@@ -1,10 +1,12 @@
 package mx.GPS.healthec;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -42,6 +44,12 @@ public class MenuActivity extends AppCompatActivity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int orientation=getResources().getConfiguration().orientation;
+        if(orientation== Configuration.ORIENTATION_PORTRAIT){
+            getSupportActionBar().hide();
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }else
+            getSupportActionBar().show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
