ac1f3d8bf79e6de4dad3da299442c83c9c3ad332
dariovidana1 <100981801+dariovidana1@users.noreply.github.com>
2023-05-26 09:13:38 -0600

    ahora sisisisisisi
    ahora si

--- Healthec/app/src/main/java/mx/GPS/healthec/ListAdapter.java
+++ Healthec/app/src/main/java/mx/GPS/healthec/ListAdapter.java
@@ -1,10 +1,11 @@
 package mx.GPS.healthec;
+
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,7 +13,10 @@ import androidx.annotation.Nullable;
 
 import java.util.List;
 
-public class ListAdapter extends ArrayAdapter <Modelo> {
+import mx.GPS.healthec.Modelo;
+import mx.GPS.healthec.R;
+
+public class ListAdapter extends ArrayAdapter<Modelo> {
 
     private List<Modelo> milista;
     private Context mContext;
@@ -20,24 +24,28 @@ public class ListAdapter extends ArrayAdapter <Modelo> {
 
     public ListAdapter(@NonNull Context context, int resource, List<Modelo> objects) {
         super(context, resource, objects);
-        this.milista=objects;
-        this.mContext=context;
-        this.resourceLayout=resource;
+        this.milista = objects;
+        this.mContext = context;
+        this.resourceLayout = resource;
     }
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View view=convertView;
-        if(view==null)
-            view= LayoutInflater.from(mContext).inflate(R.layout.item_row,null);
+        View view = convertView;
+        if (view == null) {
+            view = LayoutInflater.from(mContext).inflate(R.layout.item_row, null);
+        }
 
-        Modelo modelo= milista.get(position);
-        TextView textoact= view.findViewById(R.id.txtactividad);
+        Modelo modelo = milista.get(position);
+        TextView textoact = view.findViewById(R.id.txtactividad);
         textoact.setText(modelo.getNombreactividad());
-        TextView textoprioridad= view.findViewById(R.id.txtprioridad);
+        textoact.setTextColor(Color.BLACK); // Cambia el color del texto a negro
+
+        TextView textoprioridad = view.findViewById(R.id.txtprioridad);
         textoprioridad.setText(modelo.getPrioridadactividad());
+        textoprioridad.setTextColor(Color.BLACK); // Cambia el color del texto a negro
+
         return view;
     }
-
-
 }
