0a6ca94269d0e57c537cc1e779fc20702297d082
Fernando Pérez Romero <alu.19130959@correo.itlalaguna.edu.mx>
2023-05-26 09:18:55 -0600

    Revert "Merge branch 'main' of https://github.com/Carma223/Healthec"
    This reverts commit 430295a203257a241f72ea72f539feba3b678d20, reversing
    changes made to 1a68bd6a9e0fab031d4688560a70001fc255937f.

--- Healthec/app/src/main/java/mx/GPS/healthec/ListAdapter.java
+++ Healthec/app/src/main/java/mx/GPS/healthec/ListAdapter.java
@@ -1,11 +1,10 @@
 package mx.GPS.healthec;
-
 import android.content.Context;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,10 +12,7 @@ import androidx.annotation.Nullable;
 
 import java.util.List;
 
-import mx.GPS.healthec.Modelo;
-import mx.GPS.healthec.R;
-
-public class ListAdapter extends ArrayAdapter<Modelo> {
+public class ListAdapter extends ArrayAdapter <Modelo> {
 
     private List<Modelo> milista;
     private Context mContext;
@@ -24,28 +20,24 @@ public class ListAdapter extends ArrayAdapter<Modelo> {
 
     public ListAdapter(@NonNull Context context, int resource, List<Modelo> objects) {
         super(context, resource, objects);
-        this.milista = objects;
-        this.mContext = context;
-        this.resourceLayout = resource;
+        this.milista=objects;
+        this.mContext=context;
+        this.resourceLayout=resource;
     }
-
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View view = convertView;
-        if (view == null) {
-            view = LayoutInflater.from(mContext).inflate(R.layout.item_row, null);
-        }
+        View view=convertView;
+        if(view==null)
+            view= LayoutInflater.from(mContext).inflate(R.layout.item_row,null);
 
-        Modelo modelo = milista.get(position);
-        TextView textoact = view.findViewById(R.id.txtactividad);
+        Modelo modelo= milista.get(position);
+        TextView textoact= view.findViewById(R.id.txtactividad);
         textoact.setText(modelo.getNombreactividad());
-        textoact.setTextColor(Color.BLACK); // Cambia el color del texto a negro
-
-        TextView textoprioridad = view.findViewById(R.id.txtprioridad);
+        TextView textoprioridad= view.findViewById(R.id.txtprioridad);
         textoprioridad.setText(modelo.getPrioridadactividad());
-        textoprioridad.setTextColor(Color.BLACK); // Cambia el color del texto a negro
-
         return view;
     }
+
+
 }
